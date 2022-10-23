821775e4b68c6584435974e1cc3ad47cdf98e28a
Maciej Franikowski <mfranikowski@gmail.com>
2022-10-23 18:11:28 +0200

    Create multiple master-slave db containers

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -4,10 +4,44 @@ import { TaskEntity } from './database/entities/Task';
 import { TestEntity } from './database/entities/Test';
 import { UserEntity } from './database/entities/User';
 
+// export const AppDataSource = new DataSource({
+//   type: 'mysql',
+//   host: 'database',
+//   port: 3306,
+//   username: 'test',
+//   password: 'test',
+//   database: 'test',
+//   synchronize: true,
+//   logging: false,
+//   entities: [UserEntity, TestEntity, TaskEntity],
+//   migrations: [],
+//   subscribers: [],
+// });
 export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'development.db',
-  synchronize: true,
+  type: 'mysql',
+  replication: {
+    master: {
+      host: "databaseMaster",
+      port: 3306,
+      username: "test",
+      password: "test",
+      database: "test",
+    },
+    slaves: [{
+      host: "databaseSlave1",
+      port: 3306,
+      username: "test",
+      password: "test",
+      database: "test"
+    }, {
+      host: "databaseSlave2",
+      port: 3306,
+      username: "test",
+      password: "test",
+      database: "test"
+    }]
+  },
+  synchronize:true,
   logging: false,
   entities: [UserEntity, TestEntity, TaskEntity],
   migrations: [],
