77efdcce08ef5a3d21b44680d3cffa201e4ec80e
Maciej Franikowski <mfranikowski@gmail.com>
2022-12-22 21:19:40 +0100

    Delete troublesome line from replication script

--- database/scripts/common_funs.sh
+++ database/scripts/common_funs.sh
@@ -10,7 +10,7 @@ function create_slave_replication_user_on_master() {
     REPL_PASSWORD=$6
 
     ## There's apparently a bug here so privileges have to be flushed beforehand
-    MYSQL_QUERY="DROP USER \"$REPL_USER\"@\"$SLAVE_HOST_IP\";"
+    # MYSQL_QUERY="DROP USER \"$REPL_USER\"@\"$SLAVE_HOST_IP\";"
     mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" -e "$MYSQL_QUERY"
     MYSQL_QUERY="FLUSH PRIVILEGES;"
     mysql --user="$MASTER_ROOT_USER" --password="$MASTER_ROOT_PASSWORD" --host="$MASTER_HOST_IP" -e "$MYSQL_QUERY"
