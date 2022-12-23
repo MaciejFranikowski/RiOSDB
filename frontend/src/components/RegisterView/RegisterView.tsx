74babca6ca6a1a8e3034bbe7900e79351f39207c
Maciej Franikowski <mfranikowski@gmail.com>
2022-12-23 14:03:56 +0100

    Add frontend validation for registration fields

--- frontend/src/components/RegisterView/RegisterView.tsx
+++ frontend/src/components/RegisterView/RegisterView.tsx
@@ -10,11 +10,67 @@ function RegisterView() {
   const [details, setDetails] = useState<UserDetails>({
     firstName: '', lastName: '', email: '', password: '',
   });
+  const [errors, setErrors] = useState({
+    firstName: '', lastName: '', password: '',
+  });
+  const setFieldFirstName = (value: string) => {
+    setDetails({
+      ...details,
+      firstName: value,
+    });
+    if (errors.firstName) {
+      setErrors({
+        ...errors,
+        firstName: '',
+      });
+    }
+  };
+  const setFieldLastName = (value: string) => {
+    setDetails({
+      ...details,
+      lastName: value,
+    });
+    if (errors.lastName) {
+      setErrors({
+        ...errors,
+        lastName: '',
+      });
+    }
+  };
+  const setFieldPassword = (value: string) => {
+    setDetails({
+      ...details,
+      password: value,
+    });
+    if (errors.password) {
+      setErrors({
+        ...errors,
+        password: '',
+      });
+    }
+  };
   const [correctPassword, setCorrectPassword] = useState(false);
+  const validateForm = () => {
+    const {
+      firstName, lastName, password,
+    } = details;
+    const newErrors = {
+      firstName: '', lastName: '', password: '',
+    };
+    if (firstName === '' || !/^[a-zA-Z()]+$/.test(firstName)) { newErrors.firstName = 'Please make sure that the first name is only letters'; }
+    if (lastName === '' || !/^[a-zA-Z()]+$/.test(lastName)) { newErrors.lastName = 'Please make sure that the last name is only letters'; }
+    if (password === '' || password.length < 8) { newErrors.password = 'Please make sure that the at least 8 characters long'; }
+    return newErrors;
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    await registerUser(details);
+    const formErrors = validateForm();
+    if (formErrors.firstName !== '' || formErrors.lastName !== '') {
+      setErrors(formErrors);
+    } else {
+      await registerUser(details);
+    }
   };
 
   return (
@@ -26,12 +82,30 @@ function RegisterView() {
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formFirstName">
               <Form.Label>First name</Form.Label>
-              <Form.Control required type="text" placeholder="Enter your first name" onChange={(event) => setDetails({ ...details, firstName: event.target.value })} />
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter your first name"
+                onChange={(event) => setFieldFirstName(event.target.value)}
+                isInvalid={errors.firstName !== ''}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.firstName}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-4" controlId="formLastName">
               <Form.Label>Last name</Form.Label>
-              <Form.Control required type="text" placeholder="Enter your last name" onChange={(event) => setDetails({ ...details, lastName: event.target.value })} />
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter your last name"
+                onChange={(event) => setFieldLastName(event.target.value)}
+                isInvalid={errors.lastName !== ''}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.lastName}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
@@ -43,7 +117,16 @@ function RegisterView() {
             <div className="divider" />
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control required type="password" placeholder="Password" onChange={(event) => setDetails({ ...details, password: event.target.value })} />
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                onChange={(event) => setFieldPassword(event.target.value)}
+                isInvalid={errors.password !== ''}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Enter password again</Form.Label>
